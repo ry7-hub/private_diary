828b2512bab61bf44f253f316de3f26794232540
Ryuichi Yuasa <ry0714win@gmail.com>
2020-09-07 20:48:50 +0900

    日記一覧ページ追加

--- diary/views.py
+++ diary/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 import logging
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import InquiryForm
+from .models import Diary
 
 logger = logging.getLogger(__name__)
 
@@ -25,3 +27,12 @@ class InquiryView(generic.FormView):
         messages.success(self.request, 'メッセージを送信しました。')
         logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
         return super().form_valid(form)
+
+
+class DiaryListView(LoginRequiredMixin, generic.ListView):
+    model = Diary
+    template_name = 'diary_list.html'
+
+    def get_queryset(self):
+        diaries = Diary.objects.filter(user=self.request.user).order_by('-create_at')
+        return diaries
